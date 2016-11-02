8a9d5cd9395060fad3a3b9e562bc8ed400426e11
Johannes Steu <hello@johannessteu.de>
2016-11-02 20:25:41 +0100

    TASK: Move to psr-2 code style

--- Classes/Error/GraylogExceptionHandler.php
+++ Classes/Error/GraylogExceptionHandler.php
@@ -12,34 +12,37 @@ use Yeebase\Graylog\GraylogService;
 /**
  * Production Exception handler that reports exceptions to a Graylog server using the official gelf-php library
  */
-class GraylogExceptionHandler extends ProductionExceptionHandler {
+class GraylogExceptionHandler extends ProductionExceptionHandler
+{
 
-	/**
-	 * @var GraylogService
-	 */
-	protected $graylogService;
+    /**
+     * @var GraylogService
+     */
+    protected $graylogService;
 
-	/**
-	 * @param \Exception $exception
-	 * @return void
-	 */
-	protected function echoExceptionWeb($exception) {
-		if ($this->graylogService === NULL) {
-			$this->graylogService = new GraylogService();
-		}
-		$this->graylogService->logException($exception);
-		parent::echoExceptionWeb($exception);
-	}
+    /**
+     * @param \Exception $exception
+     * @return void
+     */
+    protected function echoExceptionWeb($exception)
+    {
+        if ($this->graylogService === NULL) {
+            $this->graylogService = new GraylogService();
+        }
+        $this->graylogService->logException($exception);
+        parent::echoExceptionWeb($exception);
+    }
 
-	/**
-	 * @param \Exception $exception The exception
-	 * @return void
-	 */
-	protected function echoExceptionCli($exception) {
-		if ($this->graylogService === NULL) {
-			$this->graylogService = new GraylogService();
-		}
-		$this->graylogService->logException($exception);
-		parent::echoExceptionCli($exception);
-	}
+    /**
+     * @param \Exception $exception The exception
+     * @return void
+     */
+    protected function echoExceptionCli($exception)
+    {
+        if ($this->graylogService === NULL) {
+            $this->graylogService = new GraylogService();
+        }
+        $this->graylogService->logException($exception);
+        parent::echoExceptionCli($exception);
+    }
 }
--- Classes/GraylogService.php
+++ Classes/GraylogService.php
@@ -25,124 +25,127 @@ use TYPO3\Party\Domain\Service\PartyService;
  *
  * @Flow\Scope("singleton")
  */
-class GraylogService {
+class GraylogService
+{
 
-	/**
-	 * @Flow\Inject
-	 * @var Context
-	 */
-	protected $securityContext;
+    /**
+     * @Flow\Inject
+     * @var Context
+     */
+    protected $securityContext;
 
-	/**
-	 * @Flow\Inject
-	 * @var ObjectManagerInterface
-	 */
-	protected $objectManager;
+    /**
+     * @Flow\Inject
+     * @var ObjectManagerInterface
+     */
+    protected $objectManager;
 
-	/**
-	 * @Flow\Inject
-	 * @var PersistenceManagerInterface
-	 */
-	protected $persistenceManager;
+    /**
+     * @Flow\Inject
+     * @var PersistenceManagerInterface
+     */
+    protected $persistenceManager;
 
-	/**
-	 * @var array
-	 */
-	protected $settings;
+    /**
+     * @var array
+     */
+    protected $settings;
 
-	/**
-	 * @param array $settings
-	 * @return void
-	 */
-	public function injectSettings(array $settings) {
-		$this->settings = $settings;
-	}
+    /**
+     * @param array $settings
+     * @return void
+     */
+    public function injectSettings(array $settings)
+    {
+        $this->settings = $settings;
+    }
 
-	/**
-	 * @param \Exception $exception
-	 * @return void
-	 */
-	public function logException(\Exception $exception) {
-		if (!isset($this->settings['host']) || strlen($this->settings['host']) === 0) {
-			return;
-		}
-		$statusCode = NULL;
-		if ($exception instanceof FlowException) {
-			$statusCode = $exception->getStatusCode();
-		}
-		// skip exceptions with status codes matching "skipStatusCodes" setting
-		if (isset($this->settings['skipStatusCodes']) && in_array($statusCode, $this->settings['skipStatusCodes'])) {
-			return;
-		}
-		$host = $this->settings['host'];
-		$port = isset($this->settings['port']) ? $this->settings['port'] : UdpTransport::DEFAULT_PORT;
+    /**
+     * @param \Exception $exception
+     * @return void
+     */
+    public function logException(\Exception $exception)
+    {
+        if (!isset($this->settings['host']) || strlen($this->settings['host']) === 0) {
+            return;
+        }
+        $statusCode = NULL;
+        if ($exception instanceof FlowException) {
+            $statusCode = $exception->getStatusCode();
+        }
+        // skip exceptions with status codes matching "skipStatusCodes" setting
+        if (isset($this->settings['skipStatusCodes']) && in_array($statusCode, $this->settings['skipStatusCodes'])) {
+            return;
+        }
+        $host = $this->settings['host'];
+        $port = isset($this->settings['port']) ? $this->settings['port'] : UdpTransport::DEFAULT_PORT;
 
-		// set chunk size option to wan (default) or lan
-		if (isset($this->settings['chunksize']) && strtolower($this->settings['chunksize']) === 'lan') {
-			$chunkSize = UdpTransport::CHUNK_SIZE_LAN;
-		} else {
-			$chunkSize = UdpTransport::CHUNK_SIZE_WAN;
-		}
+        // set chunk size option to wan (default) or lan
+        if (isset($this->settings['chunksize']) && strtolower($this->settings['chunksize']) === 'lan') {
+            $chunkSize = UdpTransport::CHUNK_SIZE_LAN;
+        } else {
+            $chunkSize = UdpTransport::CHUNK_SIZE_WAN;
+        }
 
-		// setup connection to graylog server
-		$transport = new UdpTransport($host, $port, $chunkSize);
-		$publisher = new Publisher();
-		$publisher->addTransport($transport);
+        // setup connection to graylog server
+        $transport = new UdpTransport($host, $port, $chunkSize);
+        $publisher = new Publisher();
+        $publisher->addTransport($transport);
 
-		// set logLevel depending on http status code
-		$logLevel = 4; // warning
-		if ($statusCode === 500) {
-			$logLevel = 3; // error
-		}
+        // set logLevel depending on http status code
+        $logLevel = 4; // warning
+        if ($statusCode === 500) {
+            $logLevel = 3; // error
+        }
 
-		// build message context
-		$messageContext = array(
-			'exception' => $exception,
-			'reference_code' => $exception instanceof FlowException ? $exception->getReferenceCode() : NULL,
-			'response_status' => $statusCode,
-			'short_message' => sprintf('%d %s', $statusCode, Response::getStatusMessageByCode($statusCode)),
-			'code' => $exception->getCode(),
-			'file' => $exception->getFile(),
-			'line' => $exception->getLine()
-		);
-		if ($this->securityContext !== NULL && $this->securityContext->isInitialized()) {
-			$account = $this->securityContext->getAccount();
-			if ($account !== NULL) {
-				$messageContext['authenticated_account'] = $account->getAccountIdentifier() . ' (' . $this->persistenceManager->getIdentifierByObject($account) . ')';
-				$messageContext['authenticated_roles'] = implode(', ', array_keys($this->securityContext->getRoles()));
-				if ($this->objectManager->isRegistered(PartyService::class)) {
-					/** @var PartyService $partyService */
-					$partyService = $this->objectManager->get(PartyService::class);
-					$person = $partyService->getAssignedPartyOfAccount($account);
-					if ($person instanceof Person) {
-						$messageContext['authenticated_person'] = (string)$person->getName() . ' (' . $this->persistenceManager->getIdentifierByObject($person) . ')';
-					}
-				}
-			}
-		}
+        // build message context
+        $messageContext = array(
+            'exception' => $exception,
+            'reference_code' => $exception instanceof FlowException ? $exception->getReferenceCode() : NULL,
+            'response_status' => $statusCode,
+            'short_message' => sprintf('%d %s', $statusCode, Response::getStatusMessageByCode($statusCode)),
+            'code' => $exception->getCode(),
+            'file' => $exception->getFile(),
+            'line' => $exception->getLine()
+        );
+        if ($this->securityContext !== NULL && $this->securityContext->isInitialized()) {
+            $account = $this->securityContext->getAccount();
+            if ($account !== NULL) {
+                $messageContext['authenticated_account'] = $account->getAccountIdentifier() . ' (' . $this->persistenceManager->getIdentifierByObject($account) . ')';
+                $messageContext['authenticated_roles'] = implode(', ', array_keys($this->securityContext->getRoles()));
+                if ($this->objectManager->isRegistered(PartyService::class)) {
+                    /** @var PartyService $partyService */
+                    $partyService = $this->objectManager->get(PartyService::class);
+                    $person = $partyService->getAssignedPartyOfAccount($account);
+                    if ($person instanceof Person) {
+                        $messageContext['authenticated_person'] = (string)$person->getName() . ' (' . $this->persistenceManager->getIdentifierByObject($person) . ')';
+                    }
+                }
+            }
+        }
 
-		// prepare request details
-		if (Bootstrap::$staticObjectManager instanceof ObjectManagerInterface) {
-			$bootstrap = Bootstrap::$staticObjectManager->get('TYPO3\Flow\Core\Bootstrap');
-			/* @var Bootstrap $bootstrap */
-			$requestHandler = $bootstrap->getActiveRequestHandler();
-			if ($requestHandler instanceof HttpRequestHandlerInterface) {
-				$request = $requestHandler->getHttpRequest();
-				$requestData = array(
-					'request_domain' => $request->getHeader('Host'),
-					'request_remote_addr' => $request->getClientIpAddress(),
-					'request_path' => $request->getRelativePath(),
-					'request_uri' => $request->getUri()->getPath(),
-					'request_user_agent' => $request->getHeader('User-Agent'),
-					'request_method' => $request->getMethod(),
-					'request_port' => $request->getPort()
-				);
-				$messageContext = array_merge($messageContext, $requestData);
-			}
-		}
+        // prepare request details
+        if (Bootstrap::$staticObjectManager instanceof ObjectManagerInterface) {
+            $bootstrap = Bootstrap::$staticObjectManager->get(Bootstrap::class);
+            /* @var Bootstrap $bootstrap */
+            $requestHandler = $bootstrap->getActiveRequestHandler();
+            if ($requestHandler instanceof HttpRequestHandlerInterface) {
+                $request = $requestHandler->getHttpRequest();
+                $requestData = array(
+                    'request_domain' => $request->getHeader('Host'),
+                    'request_remote_addr' => $request->getClientIpAddress(),
+                    'request_path' => $request->getRelativePath(),
+                    'request_uri' => $request->getUri()->getPath(),
+                    'request_user_agent' => $request->getHeader('User-Agent'),
+                    'request_method' => $request->getMethod(),
+                    'request_port' => $request->getPort()
+                );
+                $messageContext = array_merge($messageContext, $requestData);
+            }
+        }
 
-		// send message to graylog server
-		$logger = new Logger($publisher);
-		$logger->log($logLevel, $exception->getMessage(), $messageContext);
-	}
+        // send message to graylog server
+        $logger = new Logger($publisher);
+        $logger->log($logLevel, $exception->getMessage(), $messageContext);
+    }
 }
