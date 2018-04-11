5feb0764b4268338d1ff08527ea97511892f3106
Johannes Steu <hello@johannessteu.de>
2018-04-11 14:27:07 +0200

    TASK: Update license header

--- Classes/Error/GraylogExceptionHandler.php
+++ Classes/Error/GraylogExceptionHandler.php
@@ -1,10 +1,15 @@
 <?php
 namespace Yeebase\Graylog\Error;
 
-/*                                                                        *
- * This script belongs to the Flow package "Yeebase.Graylog".             *
- *                                                                        *
- *                                                                        */
+/**
+ * This file is part of the Yeebase.Readiness package.
+ *
+ * (c) 2018 yeebase media GmbH
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
 
 use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Error\ProductionExceptionHandler;
--- Classes/Log/GraylogLogger.php
+++ Classes/Log/GraylogLogger.php
@@ -1,6 +1,16 @@
 <?php
 namespace Yeebase\Graylog\Log;
 
+/**
+ * This file is part of the Yeebase.Readiness package.
+ *
+ * (c) 2018 yeebase media GmbH
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
 use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Log\Logger;
 use Yeebase\Graylog\GraylogService;
